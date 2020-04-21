fd2b84d7b0750e49f2086929a785fd9e6f6602ac
Akshay Sachdeva <asachdeva@citrine.io>
2020-04-20 22:20:07 -0500

    formatting code

--- src/test/scala/chapter1/Chapter1Suite.scala
+++ src/test/scala/chapter1/Chapter1Suite.scala
@@ -5,10 +5,10 @@ import cats.implicits._
 
 import munit.FunSuite
 
-class Chapter1Suite  extends FunSuite {
+class Chapter1Suite extends FunSuite {
   import Chapter1.PrintableInstances._
   import Chapter1._
-  test("Ex 1.3")  {
+  test("Ex 1.3") {
     assert(Printable.format("100") == "100")
     assert(Printable.format(100) == "100")
 
@@ -17,33 +17,33 @@ class Chapter1Suite  extends FunSuite {
   }
 
   test("Ex 1.4.6") {
-   implicit val catShow: Show[Cat] =
-     Show.show{cat =>
-         val name = cat.name.show
-         val age = cat.age.show
-         val color = cat.color.show
+    implicit val catShow: Show[Cat] =
+      Show.show { cat =>
+        val name = cat.name.show
+        val age = cat.age.show
+        val color = cat.color.show
 
-         s"$name is a $age year-old $color cat."
-  }
+        s"$name is a $age year-old $color cat."
+      }
 
-  val niobe = Cat("Niobe", 9, "black")
-  assert(niobe.show == "Niobe is a 9 year-old black cat.")
+    val niobe = Cat("Niobe", 9, "black")
+    assert(niobe.show == "Niobe is a 9 year-old black cat.")
   }
 
   test("Ex 1.5.5") {
     implicit val catEq: Eq[Cat] =
-      Eq.instance[Cat] {(cat1, cat2) =>
+      Eq.instance[Cat] { (cat1, cat2) =>
         cat1.name === cat2.name &&
         cat1.age === cat2.age &&
         cat1.color === cat2.color
       }
 
-      val cat1 = Cat("akshay", 10, "black")
-      val cat2 = Cat("akshay", 10, "black")
-      val cat3 = Cat("akshay", 9, "black")
+    val cat1 = Cat("akshay", 10, "black")
+    val cat2 = Cat("akshay", 10, "black")
+    val cat3 = Cat("akshay", 9, "black")
 
-      assert(cat1 === cat2)
-      assert(cat1 =!= cat3)
+    assert(cat1 === cat2)
+    assert(cat1 =!= cat3)
 
   }
 }
