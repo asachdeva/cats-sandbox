d6334072403a690aa1884fbe2a529338abdbcae7
Akshay Sachdeva <asachdeva@utexas.edu>
2020-05-18 16:45:49 -0500

    Adding chapter 5

--- src/main/scala/chapter5/Chapter5.scala
+++ src/main/scala/chapter5/Chapter5.scala
@@ -1,3 +1,45 @@
 package chapter5
 
-object Chapter5 {}
+object Chapter5 {
+
+  //Ex 5.4
+  object AutoBots {
+
+    import cats.data.EitherT
+    import scala.concurrent.Future
+    import scala.concurrent.ExecutionContext.Implicits.global
+    import scala.concurrent.Await
+    import scala.concurrent.duration._
+    import cats.implicits._
+
+    type Response[A] = EitherT[Future, String, A]
+
+    val powerLevels = Map("Jazz" -> 6, "Bumblebee" -> 8, "Hot Rod" -> 10)
+
+    def getPowerLevel(ally: String): Response[Int] =
+      powerLevels.get(ally) match {
+        case Some(avg) => EitherT.right(Future(avg))
+        case None      => EitherT.left(Future(s"$ally unreachable"))
+      }
+
+    def canSpecialMove(ally1: String, ally2: String): Response[Boolean] =
+      for {
+        power1 <- getPowerLevel(ally1)
+        power2 <- getPowerLevel(ally2)
+      } yield (power1 + power2) > 15
+
+    def tacticalReport(ally1: String, ally2: String): String = {
+      val stack = canSpecialMove(ally1, ally2).value
+
+      Await.result(stack, 1.second) match {
+        case Left(msg) =>
+          s"Comms error: $msg"
+        case Right(true) =>
+          s"$ally1 and $ally2 are ready to roll"
+        case Right(false) =>
+          s"$ally1 and $ally2 need a recharge"
+      }
+    }
+  }
+
+}
--- src/test/scala/chapter5/Chapter5Suite.scala
+++ src/test/scala/chapter5/Chapter5Suite.scala
@@ -1,3 +1,12 @@
 package chapter5
 
-class Chapter5Suite extends munit.FunSuite {}
+class Chapter5Suite extends munit.FunSuite {
+
+  test("Ex 5.4 -- EitherTTests") {
+    import Chapter5.AutoBots._
+
+    assert(tacticalReport("Jazz", "Bumblebee") == "Jazz and Bumblebee need a recharge")
+    assert(tacticalReport("Jazz", "Hot Rod") == "Jazz and Hot Rod are ready to roll")
+    assert(tacticalReport("Jazz", "Niobe") == "Comms error: Niobe unreachable")
+  }
+}
