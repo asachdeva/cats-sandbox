8f417ee53868a4482b09e90ef920e9974dd5f626
Akshay Sachdeva <asachdeva@utexas.edu>
2020-05-18 22:53:48 -0500

    adding FoldLeft and Foldright

--- src/main/scala/chapter7/Chapter7.scala
+++ src/main/scala/chapter7/Chapter7.scala
@@ -1,3 +1,9 @@
 package chapter7
 
-object Chapter7 {}
+object Chapter7 {
+
+  object Foldable {
+    def show[A](list: List[A]): String =
+      list.foldLeft("nil")((accum, item) => s"$item then $accum")
+  }
+}
--- src/test/scala/chapter7/Chapter7Suite.scala
+++ src/test/scala/chapter7/Chapter7Suite.scala
@@ -1,3 +1,14 @@
 package chapter7
 
-class Chapter7Suite extends munit.FunSuite {}
+class Chapter7Suite extends munit.FunSuite {
+  test("foldLeft Tests") {
+
+    import Chapter7.Foldable._
+    assert(show(Nil) == "nil")
+    assert(show(List(1, 2, 3)) == "3 then 2 then 1 then nil")
+
+    assert(List(1, 2, 3).foldLeft(List.empty[Int])((a, i) => i :: a) == List(3, 2, 1))
+    assert(List(1, 2, 3).foldRight(List.empty[Int])((i, a) => i :: a) == List(1, 2, 3))
+
+  }
+}
