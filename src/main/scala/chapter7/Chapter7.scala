c5313c4af568e7602b65ea614d10966873262dc4
Akshay Sachdeva <asachdeva@citrine.io>
2020-05-25 12:36:04 -0500

    adding fold tests

--- src/main/scala/chapter7/Chapter7.scala
+++ src/main/scala/chapter7/Chapter7.scala
@@ -5,8 +5,26 @@ object Chapter7 {
   object Foldable {
     def show[A](list: List[A]): String =
       list.foldLeft("nil")((accum, item) => s"$item then $accum")
+
+    // Ex 7.1.2
+    List(1, 2, 3).foldLeft(List.empty[Int])((a, i) => i :: a)
+    List(1, 2, 3).foldRight(List.empty[Int])((i, a) => i :: a)
+
+    // Ex 7.1.3
+    def map[A, B](list: List[A])(f: A => B): List[B] =
+      list.foldRight(List.empty[B]) { (item, accum) =>
+        f(item) :: accum
+      }
+
+    def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] =
+      list.foldRight(List.empty[B]) { (item, accum) =>
+        f(item) ::: accum
+      }
+
+    def filter[A](list: List[A])(f: A => Boolean): List[A] =
+      list.foldRight(List.empty[A]) { (item, accum) =>
+        if (f(item)) item :: accum else accum
+      }
+
   }
-
-  object WithFold {}
-
 }
--- src/test/scala/chapter7/Chapter7Suite.scala
+++ src/test/scala/chapter7/Chapter7Suite.scala
@@ -1,14 +1,21 @@
 package chapter7
 
 class Chapter7Suite extends munit.FunSuite {
-  test("foldLeft Tests") {
+  test("Ex 7.1.1 + Ex 7.1.2 Foldable Tests") {
 
     import Chapter7.Foldable._
     assert(show(Nil) == "nil")
     assert(show(List(1, 2, 3)) == "3 then 2 then 1 then nil")
 
+    assert(List(1, 2, 3).foldLeft(0)(_ - _) == -6)
+    assert(List(1, 2, 3).foldRight(0)(_ - _) == 2)
+
     assert(List(1, 2, 3).foldLeft(List.empty[Int])((a, i) => i :: a) == List(3, 2, 1))
     assert(List(1, 2, 3).foldRight(List.empty[Int])((i, a) => i :: a) == List(1, 2, 3))
+
+    assert(map(List(1, 2, 3))(_ * 2) == List(2, 4, 6))
+    assert(flatMap(List(1, 2, 3))(a => List(a, a * 10, a * 100)) == List(1, 10, 100, 2, 20, 200, 3, 30, 300))
+    assert(filter(List(1, 2, 3))(_ % 2 == 1) == List(1, 3))
 
   }
 }
